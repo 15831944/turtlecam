edff92395f69e5ca09c6b9ec804bed81fd4474df
Nicholas Gill <mythagel@gmail.com>
2014-12-22 23:04:06 +1100

    Lathe facing generated - needs to be tested.

--- lathe_face.lua
+++ lathe_face.lua
@@ -8,17 +8,18 @@ radius = diameter/2;
 
 steps = depth / step_size;
 
-move_to(radius + .1, .1);
-move_to(radius, 0);
+move_to(radius + .1, .5);
+move_to(radius, 0.5);
 print();
-for i = 0, depth, step_size do
-    turn(-90);
-    cut(step_size, 50);
-    turn(-90);
-    cut(diameter/2, 50);
-    turn(-90);
-    move(.5);
-    turn(-90);
-    move(diameter/2);
+for i = 0, steps-1 do
+    turn(90);
+    move(-0.5);
+    cut(-step_size, 50);
+    turn(90);
+    cut(radius, 50);
+    turn(90);
+    move(-.5);
+    turn(90);
+    move(radius);
     print();
 end

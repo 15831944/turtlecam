573c057026999cce22f6ea007fcdb43e3448a142
mythagel <mythagel@gmail.com>
2015-02-10 16:42:18 +1100

    Playing

--- Main.hs
+++ Main.hs
@@ -5,13 +5,21 @@ import qualified Scripting.Lua as Lua
 
 data Mode = Mill | Lathe deriving (Enum, Show)
 data Motion = Move | Cut deriving (Enum, Show)
-data Vector = Vector deriving (Show)
 
-data Turtle = Turtle { position :: Vector
+data Vector a = Vector a a a deriving (Show, Eq)
+
+plus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector x0 y0 z0) `plus` (Vector x1 y1 z1) = Vector (x0+x1) (y0+y1) (z0+z1)
+
+mult :: (Num a) => Vector a -> a -> Vector a
+(Vector x y z) `mult` a = Vector (x*a) (y*a) (z*a)
+
+
+data Turtle = Turtle { position :: Vector Double
                      , a :: Double
                      , b :: Double
                      , motion :: Maybe Motion
-                     , last_position :: Vector
+                     , last_position :: Vector Double
                      , f :: Double
                      } deriving (Show)
 
@@ -34,3 +42,4 @@ main = do
 
 pow :: Double -> Double -> IO Double
 pow d1 d2 = return $ d1 ** d2
+

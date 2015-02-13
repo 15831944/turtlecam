e14ed9f1978036c7e24cc1bf55d69c0ff5d35cfd
mythagel <mythagel@gmail.com>
2015-02-13 16:03:10 +1100

    Implementing stateful motion commands

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import qualified Scripting.Lua as Lua
 import Numeric
 import Control.Monad.State
 
+data Point a = Point a a a deriving (Show, Eq)
 
 data Vector a = Vector a a a deriving (Show, Eq)
 
@@ -21,11 +22,12 @@ r6 n = showFFloat (Just 6) n ""
 data Mode = Mill | Lathe deriving (Enum, Show)
 data Motion = Move | Cut deriving (Enum, Show)
 
-data Turtle = Turtle { position :: Vector Double
+data Turtle = Turtle { mode :: Mode
+                     , position :: Point Double
                      , a :: Double
                      , b :: Double
                      , motion :: Maybe Motion
-                     , last_position :: Vector Double
+                     , last_position :: Point Double
                      , f :: Double
                      } deriving (Show)
 
@@ -34,7 +36,7 @@ data Word = F Double
           | X Double
           | Y Double
           | Z Double
-    deriving (Eq, Read)
+    deriving (Eq, Read, Show)
 
 type Block = [Word]
 
@@ -56,9 +58,25 @@ main = do
         [file] -> doFile file
         otherwise -> usage
 
+generatePosition, generateMove, generateCut :: Point Double -> Mode -> Block
+generatePosition (Point x y z) Mill = [X x, Y y, Z z]
+generatePosition (Point x y z) Lathe = [X x, Z y]
+generateMove pos m = (G 0) : generatePosition pos m
+generateCut pos m = (G 1) : generatePosition pos m
+
+generateMotion :: Point Double -> Mode -> Motion -> Block
+generateMotion pos m Move = generateMove pos m
+generateMotion pos m Cut = generateCut pos m
+
 -- Motion commands are stateful
-move_to :: State Turtle Block
-move_to = state $ \t -> ([],t)
+-- let updatePos t pos = t { position = pos }
+-- todo update state
+move_to :: Point Double -> State Turtle Block
+move_to pos@(Point x y z) = state $ \t -> 
+    let block = generateMotion pos (mode t) Move
+    in
+        if pos /= position t then (block, t)
+        else ([],t)
 
 pow :: Double -> Double -> IO Double
 pow d1 d2 = return $ d1 ** d2

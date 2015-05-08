f77d7b4b6b85622e67ef0681a4960ad61e732bf2
Nicholas Gill <mythagel@gmail.com>
2015-05-08 22:28:03 +1000

    Working mill facing code.

--- face.lua
+++ face.lua
@@ -1,28 +1,38 @@
-w = 100;
-h = 100;
-d = 2;
-tool_d = 8;
-stepdown = 0.1;
-stepover = tool_d / 10;
+width = 100
+height = 100
+depth = 10
 
-for j = 0, (d/stepdown)-1 do
-    pitch(90)
-    cut(stepdown, 20)
-    pitch(-90)
-    if j % 2 then
-        dir = 1
-    else
-        dir = -1
-    end
-    for i = 0, h/(tool_d-stepover)/2 do
-        cut(w, 50)
-        turn(90*dir)
-        cut(tool_d - stepover, 50)
-        turn(90*dir)
-        cut(w, 50)
-        turn(-90*dir)
-        cut(tool_d - stepover, 50)
-        turn(-90*dir)
-    end
-    turn(270)
+tool_diameter = 10
+stepover = 1 -- .9 == 90%
+stepdown = 1
+
+effective_tool_width = tool_diameter * stepover
+
+width_cuts = math.ceil(width / effective_tool_width)
+depth_cuts = math.ceil(depth / stepdown)
+
+move_to(0,0,0)
+
+for z = 0, depth_cuts - 1 do
+	print("(layer " .. z+1 .. ")")
+	pitch(90)
+	cut(depth / depth_cuts, 20)
+	pitch(-90)
+
+	for h = 0, width_cuts do
+		cut(height, 50)
+		if h % 2 == 0 then
+			dir = 1
+		else
+			dir = -1
+		end
+		if h ~= width_cuts then
+			turn(90*dir)
+			cut(width / width_cuts, 50)
+			turn(90*dir)
+		end
+	end
+	turn(180)
 end
+
+turn(-180)

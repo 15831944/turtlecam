b971bdd0337111a9c95f82e99087bb3e13661633
Nicholas Gill <mythagel@gmail.com>
2015-05-12 21:43:29 +1000

    Working basic facing

--- face.lua
+++ face.lua
@@ -40,14 +40,25 @@ function rectangle(a, b, turn_left, f)
     turn(90*dir)
 end
 
-function simple()
-    for z = 0, depth_cuts - 1 do
-        print("(layer " .. z+1 .. ")")
-        pitch(90)
-        cut(depth / depth_cuts, 20)
-        pitch(-90)
-        square_zag(height, width/width_cuts, width_cuts, width_cuts % 2 ~= 0 and z % 2 ~= 0, 50)
+function plunge(dist, f)
+    pitch(90)
+    cut(dist, f)
+    pitch(-90)
+end
+
+function face(width, height, num, depth, stepdown, f, plunge_f)
+    local zig = width / num
+    local zag = height
+    local depth_passes = math.ceil(depth / stepdown)
+    local even_width = num % 2 == 0
+
+    for z = 1, depth_passes do
+        local even_layer = z % 2 == 0
+        print("( layer " .. z .. " )")
+        plunge(depth / depth_passes, plunge_f)
+        square_zag(zig, zag, num+1, not even_width and even_layer, f)
         turn(180)
+        rectangle(height, width, not even_width and not even_layer, f)
     end
 end
 
@@ -55,46 +66,14 @@ width = tonumber(argv[2] or 100)
 height = tonumber(argv[3] or 100)
 depth = tonumber(argv[4] or 1)
 
-rectangle(height, width, false, 50)
-
 print("(" .. width .. " " .. height .. " " .. depth .. ")")
 
-tool_diameter = 10
-stepover = 1 -- .9 == 90%
-stepdown = 5
+tool_diameter = 8
+stepover = .9 -- 90%
+stepdown = .5
 
 effective_tool_width = tool_diameter * stepover
-
 width_cuts = math.ceil(width / effective_tool_width)
-depth_cuts = math.ceil(depth / stepdown)
 
 move_to(0,0,0)
-for z = 0, depth_cuts - 1 do
-	print("(layer " .. z+1 .. ")")
-	pitch(90)
-	cut(depth / depth_cuts, 20)
-	pitch(-90)
-
-    if z % 2 == 0 then
-        dir = 1
-    else
-        dir = -1
-    end
-
-    left = width_cuts % 2 ~= 0 and z % 2 ~= 0
-
-    if z == 0 then
-        square_zag(width/width_cuts, height, width_cuts, left, 50)
-    else
-        if width_cuts % 2 == 0 and z % 2 ~= 0 then
-            dir = -dir
-        end
-        turn(90*dir)
-        cut(width/width_cuts, 50)
-        turn(90*dir)
-
-        left = not left
-        square_zag(width/width_cuts, height, width_cuts-1, left, 50)
-    end
-end
-
+face(width, height, width_cuts, depth, stepdown, 50, 20)

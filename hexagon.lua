aec66eb315085997efdb5d6c3df868ba003d3c5e
Nicholas Gill <mythagel@gmail.com>
2015-07-25 15:01:55 +1000

    First cut hexagon grid

--- hexagon.lua
+++ hexagon.lua
@@ -10,5 +10,26 @@ local cam = require("cam")
 --    cut(100, 50)
 --end
 
+local function deg2rad(deg)
+    return (deg/1)*(math.pi/180)
+end
+local function polygon_radius_to_edge(r, n)
+    return 2 * r * math.sin(deg2rad(180/n))
+end
+local function polygon_apothem(r, n)
+    return r * math.cos(deg2rad(180/n))
+end
 
-cam.polygon_helix({x=0,y=0,z=0}, 16/2, 6, 10, 1, 50);
+
+offset = 2;
+for y = 1, 80, 2*(polygon_apothem(8,6)+offset) do
+    for x = 0, 10 do
+        hex_x = x * (4+ polygon_radius_to_edge(8, 6) + offset);
+        hex_y = y;
+        if x % 2 == 0 then
+            hex_y = hex_y + (polygon_apothem(8,6) + offset);
+        end
+        cam.polygon_helix({x=hex_x,y=hex_y,z=0}, 16/2, 6, 10, 1, 50);
+        move_to(nil,nil,1);
+    end
+end

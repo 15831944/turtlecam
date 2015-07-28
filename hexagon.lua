a407950a2bb49139902a67f1e688ed0d1e085589
Nicholas Gill <mythagel@gmail.com>
2015-07-28 22:37:14 +1000

    Playing with hexagons

--- hexagon.lua
+++ hexagon.lua
@@ -21,8 +21,12 @@ local function polygon_apothem(r, n)
 end
 
 
-radius = 16/2;
-offset = 2;
+function cut_hex(x, y)
+    return true
+end
+
+radius = 16/2 - 1.5/2;
+offset = 4;
 hexagons_x = 10;
 hexagons_y = 10;
 depth = 1;
@@ -31,14 +35,17 @@ turns = 10;
 
 sides = 6;
 apothem = polygon_apothem(radius, sides);
-for y = 1, hexagons_y*(2*(apothem+offset)), 2*(apothem+offset) do
+for y = 1, hexagons_y*(2*(apothem+offset/2)), 2*(apothem+offset/2) do
     for x = 1, hexagons_x do
-        hex_x = x * ((radius/2) + polygon_radius_to_edge(radius, sides) + offset);
-        hex_y = y;
-        if x % 2 == 0 then
-            hex_y = hex_y + (apothem + offset);
+        if not cut_hex(x, y) then
+        else
+            hex_x = x * ((radius/2) + polygon_radius_to_edge(radius, sides) + offset);
+            hex_y = y;
+            if x % 2 == 0 then
+                hex_y = hex_y + (apothem + offset/2);
+            end
+            cam.polygon_helix({x=hex_x,y=hex_y,z=0}, radius, sides, turns, depth, f);
+            move_to(nil,nil,1);
         end
-        cam.polygon_helix({x=hex_x,y=hex_y,z=0}, radius, sides, turns, depth, f);
-        move_to(nil,nil,1);
     end
 end

f99dcfa819279caabba71a9034520b9bb1d3ef2b
Nicholas Gill <mythagel@gmail.com>
2015-08-01 13:54:30 +1000

    Use cam functions

--- basic.lua
+++ basic.lua
@@ -41,24 +41,6 @@ if false then
     end
 end
 
-function spiral(center, steps, loops, a, b, start_theta, f)
-	step = 2 * math.pi / steps;
-	max = 2 * math.pi * loops;
-
-    pos = (a + b * (0 + start_theta));
-    x = center.x + pos * math.cos(0);
-    y = center.y + pos * math.sin(0);
-    move_to(x, y, nil)
-    turn_to(0)
-	
-	for angle = 0, max, step do
-		pos = (a + b * (angle + start_theta));
-		x = center.x + pos * math.cos(angle);
-		y = center.y + pos * math.sin(angle);
-
-		cut_to(x, y, nil, f);
-    end
-end
 
 function half_circle(r, sides)
     sides = sides or 64
@@ -89,12 +71,6 @@ if false then
     end
 end
 
-function spiral2(center, radius, gap, f, steps)
-    steps = steps or 64
-    loops = radius/gap;
-    b = radius/(loops * 2*math.pi);
-    spiral(center, steps, loops, 0, b, 0, f);
-end
 
 if false then
     steps = 64;
@@ -108,22 +84,16 @@ if false then
     loops = radius/((tool_r*2)-stepover);
     b = radius/(loops * 2*math.pi);
 
-    spiral({x=0,y=0}, steps, loops, 0, b, 0);
+    cam.raw_spiral({x=0,y=0}, steps, loops, 0, b, 0);
     turn(-60)
 
     cam.circle(radius, 1, 50)
 end
 
 
-function circle_clearing(radius, center, gap, f)
-    radius = radius - gap/2
-    move_to(center.x, center.y, nil);
-    cam.polygon(radius, 64, 64, center, f)
-    spiral2(center, radius, gap, f) -- 10% stepover
-end
 
 for i = 0, 10 do
     cam.plunge(0.5, 50)
-    circle_clearing(25/2, {x=0,y=0}, 8, 50)
+    cam.circle_clearing(25/2, {x=0,y=0}, 8/2, 50)
 end
 

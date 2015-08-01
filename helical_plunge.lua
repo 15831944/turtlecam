f612e1586cfc56c2d3d6da4b61c1e0f22c5a64dc
Nicholas Gill <mythagel@gmail.com>
2015-08-01 23:30:00 +1000

    Fill out involute spiral

--- helical_plunge.lua
+++ helical_plunge.lua
@@ -12,18 +12,30 @@ function helical_plunge(r, depth, stepdown, f)
     move(r)
 end
 
-function involute()
-    x = r * (math.cos(t) + (t * math.sin(t)))
-    y = r * (math.sin(t) - (t * math.cos(t)))
+function involute_spiral(center, dir, r, steps, loops, f)
+	local step = 2 * math.pi / steps;
+	local max = 2 * math.pi * loops;
+
+	for theta = 0, max, step do
+        t = theta * dir
+        x = r * (math.cos(t) + (t * math.sin(t)))
+        y = r * (math.sin(t) - (t * math.cos(t)))
+
+		cut_to(x+center.x, y+center.y, nil, f);
+    end
 end
+
+turn(90)
+move(1)
+move(-1)
+turn(-90)
+involute_spiral(pos(), -1, math.pi/10, 64, 10, 200)
 
 tool_r = 4
 f = 200
 r = 20 - tool_r
 z = 7
 
---turn(90)
---move_to(10,10,nil)
-helical_plunge(tool_r, 7, 0.5, f)
-cam.spiral(pos(), r, 0.5, f)
-arc_to(pos().x, pos().y, nil, 1, -r, 0, 1, f)
+--helical_plunge(tool_r, 7, 0.5, f)
+--cam.spiral(pos(), r, 0.5, f)
+--arc_to(pos().x, pos().y, nil, 1, -r, 0, 1, f)

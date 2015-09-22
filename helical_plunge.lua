8fc5b758082eb0a6cf065b27af7ba4052d6537ce
Nicholas Gill <mythagel@gmail.com>
2015-09-22 22:09:14 +1000

    Helical plunge in progress

--- helical_plunge.lua
+++ helical_plunge.lua
@@ -1,14 +1,14 @@
 local cam = require("cam")
 
 --print("G02 X0 Y0 I1 J1 Z-10 P10 F10")
--- Note! if tool r >= tool_t there will be a post left in the middle
+-- Note! if r >= tool_r there will be a post left in the middle
 function helical_plunge(r, depth, stepdown, f)
     turns = depth / stepdown
-    turn_to(0)
+    p = pos();
+    -- TODO accurate arc center
+    -- offset to center is vector based on current theta
     move(-r)
-    -- TODO depth is an absolute value 
-    -- but expectation is relative...
-    arc_to(nil, nil, -depth, 1, r, 0, turns, f);
+    arc_to(nil, nil, pos().z-depth, 1, r, 0, turns, f);
     move(r)
 end
 
@@ -25,17 +25,32 @@ function involute_spiral(center, dir, r, steps, loops, f)
     end
 end
 
-turn(90)
-move(1)
-move(-1)
-turn(-90)
-involute_spiral(pos(), -1, math.pi/10, 64, 10, 200)
+
+function square(size)
+    for _ = 1, 4 do
+        move(size)
+        turn(90)
+    end
+end
+
+--for i = 1, 10 do
+--    square(1);
+--    move(1);
+--end
+--move_to(0,0,nil)
+--involute_spiral(pos(), -1, math.pi/10, 64, 10, 200)
 
 tool_r = 4
 f = 200
 r = 20 - tool_r
 z = 7
 
---helical_plunge(tool_r, 7, 0.5, f)
+for i = 1, 4 do
+    move(20)
+    p = pos()
+    helical_plunge(1, 5, 0.5, f)
+    turn(90)
+    move_to(nil, nil, p.z)
+end
 --cam.spiral(pos(), r, 0.5, f)
 --arc_to(pos().x, pos().y, nil, 1, -r, 0, 1, f)
